26c9341e19c50ca7cfb7b85120530337d48350c6
daw2-codina22 <codinalleonartferran@fpllefia.com>
2021-10-21 16:09:48 +0200

    Versio 0.3-B del projecte prjava43codina22. Branca00 modificada.

--- src/prjava43codina22/Prjava43codina22.java
+++ src/prjava43codina22/Prjava43codina22.java
@@ -7,6 +7,7 @@ package prjava43codina22;
 package prjava43codina22;
 import java.awt.*; // using AWT containers and components
 import java.awt.event.*; // using AWT events and listener interfaces
+import java.io;
 // An AWT GUI program inherits the top-level container java.awt.Frame
 public class Prjava43codina22 extends Frame
 implements ActionListener, WindowListener {
